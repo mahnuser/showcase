0a0aa30c68de4d8382416eed83c16383d6c0649d
Mahmut  Seliz <mahmutseliz-gs@hotmail.com>
2023-01-04 16:16:19 +0300

    add: some projects

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+
 let data = [
     { 
         name: "Love Meter",
@@ -26,58 +27,65 @@ let data = [
 
     },
     { 
-        name: "Love Meter",
-        desc: "Calculates similarity between words.",
-        testLink: "https://askolcer.mahnuser.dev",
-        liveLink: "https://google.com",
-        technologies: "Firebase Hosting-Firestore, TailwindCSS, Webpack",
-        date: "02/20/2020",
-        date2: "2020",
-        traffic: "5",
-        githubLink:"https://www.github.com",
-        gitlabLink: "https://www.gitlab.com",
-
-    },
-    { 
-        name: "Love Meter",
-        desc: "Calculates similarity between words.",
-        testLink: "https://askolcer.mahnuser.dev",
-        liveLink: "https://google.com",
-        technologies: "Firebase Hosting-Firestore, TailwindCSS, Webpack",
-        date: "02/20/2020",
-        date2: "2020",
-        traffic: "5",
-        githubLink:"https://www.github.com",
-        gitlabLink: "",
-
-    },
-    { 
-        name: "Love Meter",
-        desc: "Calculates similarity between words.",
-        testLink: "",
-        liveLink: "https://google.com",
-        technologies: "Firebase Hosting-Firestore, TailwindCSS, Webpack",
-        date: "02/20/2020",
-        date2: "2020",
-        traffic: "5",
-        githubLink:"https://www.github.com",
-        gitlabLink: "",
-
-    },
-    { 
-        name: "Love Meter",
-        desc: "Calculates similarity between words.",
+        name: "Auto-Cart",
+        desc: "Automates the adding games to cart process by wishlist on Steam.",
         testLink: "",
         liveLink: "",
-        technologies: "Firebase Hosting-Firestore, TailwindCSS, Webpack",
+        technologies: "Python, Selenium",
         date: "02/20/2020",
-        date2: "2020",
+        date2: "2021",
         traffic: "5",
-        githubLink:"https://www.github.com",
-        gitlabLink: "https://www.gitlab.com",
+        githubLink:"https://github.com/hyp3ri0nnn/wishlist-to-cart",
+        gitlabLink: "",
 
-    }
+    },
+
+    { 
+        name: "Book Tracker",
+        desc: "Helps to track your read books and reading times.",
+        testLink: "",
+        liveLink: "",
+        technologies: "Python, PyQt5",
+        date: "02/20/2021",
+        date2: "2021",
+        traffic: "5",
+        githubLink:"https://www.github.com/hyp3ri0nnn/book-tracker",
+        gitlabLink: "",
+
+    },
+
+    { 
+        name: "Online journal",
+        desc: "It is a journal site which takes some features from social media apps.",
+        testLink: "",
+        liveLink: "",
+        technologies: "Python, Django, PostgreSQL",
+        date: "02/20/2021",
+        date2: "2021",
+        traffic: "5",
+        githubLink:"https://www.github.com/hyp3ri0nnn/online-journal",
+        gitlabLink: "",
+
+    },
+    { 
+        name: "Firebase Chat",
+        desc: "Live support chat app builded on Firebase.",
+        testLink: "",
+        liveLink: "",
+        technologies: "Firebase-Hosting, Firestore, Javascript",
+        date: "02/20/2022",
+        date2: "2022",
+        traffic: "5",
+        githubLink:"",
+        gitlabLink: "",
+
+    },
+
+
 ]
+
+
+
 
 
 export default data;
